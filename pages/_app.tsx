88cafa04d682bfbd5d0f485ed2071e2588a7e731
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-09-03 01:42:42 +0530

    Added Context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { CounterContext } from "../context/counterContext";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = () => {
+    setCounter((counter) => counter + 1);
+  };
+  return (
+    <CounterContext.Provider value={{ counter, incrementCounter }}>
+      <Component {...pageProps} />
+    </CounterContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/test.tsx
+++ pages/test.tsx
@@ -5,8 +5,15 @@ import { NextPage, GetServerSideProps } from "next";
 import withApollo from "../lib/withApollo";
 import { useTestingQuery } from "../generated/index";
 import { useQuery } from "@apollo/client";
+import { useCounter } from "../context/counterContext";
 
 function Test(props) {
+  const { incrementCounter, counter } = useCounter();
+
+  const handleClick = () => {
+    incrementCounter();
+  };
+
   const callMe = async () => {
     const { data, loading } = await props.apollo.query({
       query: TESTING_QUERY,
@@ -17,7 +24,14 @@ function Test(props) {
     callMe();
   }, []);
   //   console.log(client)
-  return <div> Hello</div>;
+  return (
+    <div>
+      {" "}
+      Hello
+      <h1>{counter}</h1>
+      <button onClick={() => handleClick()}>Increase</button>
+    </div>
+  );
 }
 
 export default withApollo(Test, { getDataFromTree });
